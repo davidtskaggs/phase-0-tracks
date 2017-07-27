9eb9155c608e44807820c749af4453f1a2fc2725
David Skaggs <davidtskaggs@gmail.com>
2017-07-27 11:46:35 -0400

    Add initial pseudocode

---
+++ ruby/hashes.rb
@@ -0,0 +1,21 @@
+# 5.2: Data Structures
+# Release 2: pseudocode and write a program that will allow an interior designer to enter the details of a given client: the client's name, age, number of children, decor theme, and so on (you can choose your own as long as it's a good mix of string, integer, and boolean data).
+
+# declare variable client information and set it equal to an empty hash
+# ask user for client's name
+  # store name in variable (str)
+# ask user for client's age
+  # store age as variable (int)
+# ask user for client's number of children
+  # store number of children as variable (int)
+# ask user for client's decor theme
+  # store decor theme as variable (str)
+# ask user if client has pet's (true or false)
+  # store response as boolean variable
+# print hash
+# ask user if anything needs to be updated (type 'none' to exit)
+  # store response as variable
+  # if user types a symbol in the hash, ask "What is the updated value?"
+    # store response as variable
+    # update hash with updated value
+# print hash
