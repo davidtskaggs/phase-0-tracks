893bbaaa82ddcf39dd0b7805510b4d5c4addf0d7
David Skaggs <davidtskaggs@gmail.com>
2017-07-27 16:52:49 -0400

    Add release 2 for hash

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -16,35 +16,49 @@
 # end
 # # p colors
 
-# car = {ford: "taurus",
-# vw: "cc",
-# porsche: "cayenne",
-# chevy: "silverado"
-# }
+car = {ford: "taurus",
+vw: "cc",
+porsche: "cayenne",
+chevy: "silverado"
+}
 
 # car.each do |make, model|
 #   puts "My #{make} #{model} is the best car!!"
 # end
 
 # Release 2:
-num_array = [1,2,3,4,5,6,7,8,9,10]
+# num_array = [1,2,3,4,5,6,7,8,9,10]
+
+num_hash = {
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4
+}
 
 # A method that iterates through the items, deleting any that meet a certain condition (for example, deleting any numbers that are less than 5).
 
-
 # num_array.delete_if {|x| x > 5}
 
+# car.delete_if {|k, v| v == "cayenne"}
+# p car
 
 # A method that filters a data structure for only items that do satisfy a certain condition (for example, keeping any numbers that are less than 5).
 
 # num_array.keep_if {|x| x <= 4}
 
+# num_hash.keep_if {|key, value| key == :two}
+# p num_hash
 
 
 # A different method that filters a data structure for only items satisfying a certain condition -- Ruby offers several options!
 
 # even_numbers = num_array.select {|num| num.odd?}
 # p even_numbers
+
+# new_hash = num_hash.reject {|key, value| key.length > 3}
+# p new_hash
+# p num_hash
 
 # A method that will remove items from a data structure until the condition in the block evaluates to false, then stops (you may not find a perfectly working option for the hash, and that's okay).
 
