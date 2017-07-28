09b6e690a0bcfc5c27fbde7d66da759b51b1c965
David Skaggs <davidtskaggs@gmail.com>
2017-07-28 11:04:29 -0400

    Add nested data structure items

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -2,4 +2,16 @@
 # David Skaggs
 # 5.4 Nested Data Structures
 # Release 2: Design and Build a Nested Data Structure
-#
+
+bronx_zoo = {
+  exhibits = {
+    bears: ["black bear", "brown bear", "polar bear", "grizzly bear"],
+    birds: ["bali mynah", "turaco", "bee eater"],
+    sea_lion: ["california sea lion"],
+    reptiles: ["anaconda", "blue iguana", "poison dart frog"]
+  }
+  concession_stand_items = {
+    food: ["chips", "candy", "sandwiches", "ice cream", "hot dogs"],
+    drinks: ["water", "soda", "beer", "wine", "juice"]
+  }
+}
