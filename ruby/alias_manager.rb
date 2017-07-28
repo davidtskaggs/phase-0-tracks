63d9c31dd8eb274e46770ad4407585d26917d604
David Skaggs <davidtskaggs@gmail.com>
2017-07-28 11:31:01 -0400

    Add instructions

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,3 +1,10 @@
 # DBC Week 5
 # David Skaggs
 # 5.5 Solo Challenge: Manipulating Strings with Iteration
+
+=begin
+Instructions:
+  - pseudocode and write a method that takes a spy's real name (e.g., "Felicia Torres") and creates a fake name with it by doing the following:
+    1. Swapping the first and last name.
+    2. Changing all of the vowels (a, e, i, o, or u) to the next vowel in 'aeiou', and all of the consonants (everything else besides the vowels) to the next consonant in the alphabet. So 'a' would become 'e', 'u' would become 'a', and 'd' would become 'f'.
+=end
