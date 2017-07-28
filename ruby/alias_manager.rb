04020e6ad1f7422352ef628c6501cfe8def39bce
David Skaggs <davidtskaggs@gmail.com>
2017-07-28 11:45:10 -0400

    Add pseudocode

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -8,3 +8,23 @@ Instructions:
     1. Swapping the first and last name.
     2. Changing all of the vowels (a, e, i, o, or u) to the next vowel in 'aeiou', and all of the consonants (everything else besides the vowels) to the next consonant in the alphabet. So 'a' would become 'e', 'u' would become 'a', and 'd' would become 'f'.
 =end
+
+# Pseudocode:
+# Input: String
+# Output: String
+
+=begin
+Business Logic
+
+- define method that takes a string, and returns a lower case array of individual characters
+- create a method that has one parameter; When called, this method should take a array as an argument.
+- define a string variable that is equal to all vowels
+- create map! block to iterate through the letters in the array
+  - IF the letter equal "z", replace it with a 'b'
+  - ELSIF the letter equals a "u", replace it with an "a"
+  - ELSIF the letter is a consonant, call the .next method
+  - ELSE it is going to replace the vowel with the next vowel in the string variable
+  - each block then returns an array of string letters
+- define method that takes array of string letters
+  - this method returns a string with the first letter in each word capitalized
+=end
