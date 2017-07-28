37b5c9bd2828c58349ed20eda369a3657c743d82
David Skaggs <davidtskaggs@gmail.com>
2017-07-28 11:10:54 -0400

    fix bug in bronx_zoo

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -4,20 +4,21 @@
 # Release 2: Design and Build a Nested Data Structure
 
 bronx_zoo = {
-  exhibits = {
+  exhibits: {
     bears: ["black bear", "brown bear", "polar bear", "grizzly bear"],
     birds: ["bali mynah", "turaco", "bee eater"],
     sea_lion: ["california sea lion"],
     reptiles: ["anaconda", "blue iguana", "poison dart frog"]
   },
-  admission_price = {
+  admission_price: {
     adult: 36.95,
     child: 26.95,
     senior: 31.95,
     annual_membership: 129.95
   },
-  concession_stand_items = {
+  concession_stand_items: {
     food: ["chips", "candy", "sandwiches", "ice cream", "hot dogs"],
     drinks: ["water", "soda", "beer", "wine", "juice"]
   }
 }
+p bronx_zoo
