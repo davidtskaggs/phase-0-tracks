32e11a2f9d8b547896e04f77f340924ac18d192a
David Skaggs <davidtskaggs@gmail.com>
2017-07-31 12:35:24 -0400

    Add mixin module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -10,8 +10,26 @@
 # Shout.yell_happily("Howdy!")
 # Shout.yell_angrily("Get off my lawn")
 
-class Happy
+module Shout
+  def shouting(phrase)
+    puts "#{phrase.upcase}!!!!"
+  end
 end
 
-class Angry
+class IsleyBrothers
+  include Shout
 end
+
+class PopStar
+  include Shout
+end
+
+new_song = IsleyBrothers.new
+new_song.shouting("Come on and shout now!")
+new_song.shouting("Hey hey hey hey")
+new_song.shouting("A little bit softer now...")
+new_song.shouting("A little bit louder now... ")
+
+justin_timberlake = PopStar.new
+justin_timberlake.shouting("hey everybody :)")
+justin_timberlake.shouting("lets make some noise ...")
