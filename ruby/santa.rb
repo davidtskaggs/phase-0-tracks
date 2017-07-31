a920f69b8fc26fcc755cfc446b4a684095f7dd76
David Skaggs <davidtskaggs@gmail.com>
2017-07-31 10:19:02 -0400

    Add release 0

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -9,3 +9,17 @@
   # - a 'speak' method that will print "Ho, ho, ho! Haaaappy Holidays!"
   # - a 'eat_milk_and_cookies' method that takes a cookie type as a parameter and prints "That was a good [type of cookie]"
   # - a initialize method that prints "initializing Santa instance"
+
+class Santa
+  def initialize
+    "initializing Santa instance..."
+  end
+
+  def speak
+    "Ho, ho ho! Haaaappy Holidays!"
+  end
+
+  def eat_milk_and_cookies(cookie_type)
+    "That was a good #{cookie_type}"
+  end
+end
