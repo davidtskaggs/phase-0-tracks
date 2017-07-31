d284b5f74a1cc60522ec73d959772b6b837e1757
David Skaggs <davidtskaggs@gmail.com>
2017-07-31 10:16:46 -0400

    Add pseudocode

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -3,3 +3,9 @@
 # David Skaggs
 
 # Release 0: Bring Santa to Life
+# Pseudocode:
+# Define a Santa class
+# Class should have three instance methods:
+  # - a 'speak' method that will print "Ho, ho, ho! Haaaappy Holidays!"
+  # - a 'eat_milk_and_cookies' method that takes a cookie type as a parameter and prints "That was a good [type of cookie]"
+  # - a initialize method that prints "initializing Santa instance"
