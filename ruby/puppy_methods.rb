3878dd54600ef8b5061903cc3a59254174f547e3
David Skaggs <davidtskaggs@gmail.com>
2017-08-02 13:21:19 -0400

    Add class and iteration

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,5 +1,9 @@
 class Puppy
 
+  def initialize
+    puts "Initializing new puppy instance ..."
+  end
+
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
@@ -24,9 +28,55 @@ class Puppy
   end
 end
 
-puppy_new = Puppy.new
-puppy_new.fetch("ball")
-puppy_new.speak(5)
-puppy_new.roll_over
-p puppy_new.dog_years(5)
-puppy_new.shake_hands
+# puppy_new = Puppy.new
+# puppy_new.fetch("ball")
+# puppy_new.speak(5)
+# puppy_new.roll_over
+# p puppy_new.dog_years(5)
+# puppy_new.shake_hands
+
+class Boat
+
+  def initialize(boat_type, color)
+    @boat_type = boat_type
+    @color = color
+    puts "Initializing a new #{color} #{boat_type}"
+  end
+
+  def change_boat_type=(new_boat_type)
+    @boat_type = new_boat_type
+    puts "Your boat is now a #{@boat_type}"
+  end
+
+  def boat
+    @boat_type
+  end
+
+  def board_boat(int)
+    puts "#{int} people have boarded the boat"
+  end
+
+  def sail_around_lake(number)
+    number.times do
+    puts "You've sailed around the lake!"
+    end
+  end
+end
+
+
+# sailboat = Boat.new('power boat', 'blue')
+# sailboat.change_boat_type = "sailboat"
+# sailboat.boat
+# sailboat.board_boat(5)
+# sailboat.sail_around_lake(3)
+
+sailboats = []
+
+50.times do |num|
+  sailboats << Boat.new("sailboat", "white")
+end
+
+sailboats.each do |boat|
+  boat.board_boat(2)
+  boat.sail_around_lake(1)
+end
