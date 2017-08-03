1472ceb36f86d92b8cec2dbdf251d0efb80a3c8d
David Skaggs <davidtskaggs@gmail.com>
2017-08-03 14:01:05 -0400

    Update new_list method

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -11,33 +11,23 @@
 # output: [what data type goes here, array or hash?]
 
 
-def new_list(string, quantity = 1)
-  $list = []
-  list_item = {
-    quantity: quantity,
-    item: string
-  }
-  # $list = string.split
-  $list.push(list_item)
+def new_list(string)
+  items = string.split(" ")
+  list_item = {}
 
-  # $list.each do |list_item|
-  #   list_item[:quantity] = 1
-  # end
-  # list_item = {
-  #   quantity: quantity,
-  #   item: string
-  # }
-  # $list.push(list_item)
-  # print_list
+  items.each do |item|
+    list_item[item] = 1
+  end
+  list_item
 end
 
-def add_to_list (item, quantity = 1)
-  list_item = {
-    quantity: quantity,
-    item: item
-  }
-  $list.push(list_item)
-end
+# def add_to_list (item, quantity = 1)
+#   list_item = {
+#     quantity: quantity,
+#     item: item
+#   }
+#   $list.push(list_item)
+# end
 
 
 # Method to add an item to a list
@@ -49,9 +39,9 @@ end
 # output: array
 
 
-def remove_from_list (item)
-  $list.delete_if{|i| i[:item] == item}
-end
+# def remove_from_list (item)
+#   $list.delete_if{|i| i[:item] == item}
+# end
 
 
 # Method to remove an item from the list
@@ -60,13 +50,13 @@ end
 # output: remove the item from the list.
 
 
-def update_quantity (quantity, item)
-  $list.each do |list_item|
-    if list_item[:item] == item
-      list_item[:quantity] = quantity
-    end
-  end
-end
+# def update_quantity (quantity, item)
+#   $list.each do |list_item|
+#     if list_item[:item] == item
+#       list_item[:quantity] = quantity
+#     end
+#   end
+# end
 
 # Method to update the quantity of an item
 # input: item. quantity
@@ -75,9 +65,9 @@ end
 # output: update the quantity for the item
 
 
-def print_list
-  $list.each {|list_item| puts "#{list_item[:quantity]} #{list_item[:item]}"}
-end
+# def print_list
+#   $list.each {|list_item| puts "#{list_item[:quantity]} #{list_item[:item]}"}
+# end
 
 # Method to print a list and make it look pretty
 # input: global array
@@ -86,19 +76,19 @@ end
 
 #new_list("carrots apples cereal pizza")
 
-new_list("Onions")
-# $list
-add_to_list("Lemonade", 2)
-add_to_list("Tomatoes", 3)
-add_to_list("Ice Cream", 4)
+shopping_list = new_list("Onions apples")
+p shopping_list
+# add_to_list("Lemonade", 2)
+# add_to_list("Tomatoes", 3)
+# add_to_list("Ice Cream", 4)
 # p $list
 # puts $list.length
-remove_from_list("Lemonade")
+# remove_from_list("Lemonade")
 # puts $list.length
 
 # add_to_list("apples", 15)
 # print_list
 # add_to_list("carrots", 12)
-update_quantity(1, "Ice Cream")
+# update_quantity(1, "Ice Cream")
 # puts
-print_list
+# print_list
