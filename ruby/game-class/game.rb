f91bcfa841c9b9248d4df99e8e0ef22a54a885db
David Skaggs <davidtskaggs@gmail.com>
2017-08-04 14:38:19 -0400

    Method update

--- ruby/game-class/game.rb
+++ ruby/game-class/game.rb
@@ -44,9 +44,7 @@ class HangMan
   end
 
   def welcome
-    puts "Welcome to the Hangman game!"
-  puts "----------------------------"
-  puts "Guess a Letter:"
+    "Welcome to the Hangman game!"
   end
 
   def display_word_dash_str
@@ -89,16 +87,16 @@ end
 
 # User Interface
 
-new_game = HangMan.new("abcd")
-new_game.welcome
-new_game.display_word_dash_str
-puts "'#{new_game.display_word_dash_str}'"
-puts "Total Guesses: #{new_game.word_dash_str.length}"
+# new_game = HangMan.new("abcd")
+# new_game.welcome
+# new_game.display_word_dash_str
+# puts "'#{new_game.display_word_dash_str}'"
+# puts "Total Guesses: #{new_game.word_dash_str.length}"
 
-new_game.letter_guesses("a")
-new_game.letter_guesses("b")
-new_game.letter_guesses("c")
-new_game.letter_guesses("")
-new_game.letter_guesses("a")
-new_game.letter_guesses("d")
-new_game.is_game_over
+# new_game.letter_guesses("a")
+# new_game.letter_guesses("b")
+# new_game.letter_guesses("c")
+# new_game.letter_guesses("")
+# new_game.letter_guesses("a")
+# new_game.letter_guesses("d")
+# new_game.is_game_over
