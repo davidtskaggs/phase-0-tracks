8fbd1828736885d542e608a254cafadace67d3ba
David Skaggs <davidtskaggs@gmail.com>
2017-08-05 12:00:00 -0400

    Add pseudocode

--- js/explore.js
+++ js/explore.js
@@ -1,2 +1,10 @@
 // 7.1 JS Syntax
 // Release 4: Write a JavaScript Program
+// Write a function that takes a string as a parameter and returns a reversed string
+// Pseudocode:
+// - declare function and give it a single parameter
+// - crete an empty variable that will be used as the reversed string
+// - create for loop
+  // - iterate though the string
+  // - add each letter to the return variable in reverse order
+// return the reversed string
