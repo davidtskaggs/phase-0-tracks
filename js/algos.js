fd2ae8736944fd771ffd457eb437e4b8bdce16da
David Skaggs <davidtskaggs@gmail.com>
2017-08-07 11:28:34 -0400

    Add release 0

--- js/algos.js
+++ js/algos.js
@@ -1,5 +1,18 @@
 // 7.3 Solo Challenge
 
+// Release 0:Find the Longest Phrase
+arrOfWords = ["long word", "longest word", "longer word"]
+
+function findLongestWord(arr) {
+  arr.sort(function(a,b) {
+    return b.length - a.length
+  });
+  var biggestWord = arr[0];
+  return biggestWord;
+}
+
+findLongestWord(arrOfWords)
+
 // Release 2: Generate Random Test Data
 alphabet = ['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m']
 
