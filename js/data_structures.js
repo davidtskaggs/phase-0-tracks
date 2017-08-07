f0c7d9f20f7e1a6a41a2e7b813a5c8b7a4cc35cc
David Skaggs <davidtskaggs@gmail.com>
2017-08-07 10:33:10 -0400

    Add function

--- js/data_structures.js
+++ js/data_structures.js
@@ -7,3 +7,15 @@ console.log(colors);
 
 horseNames.push("bob");
 console.log(horseNames);
+
+function createHorseObj(arr1, arr2) {
+  var horseObj = {};
+
+  for(var i = 0; i <= arr1.length - 1; i++) {
+    console.log(arr1[i]);
+    horseObj[arr1[i]] = arr2[i];
+  }
+  console.log(horseObj);
+}
+
+createHorseObj(horseNames, colors)
