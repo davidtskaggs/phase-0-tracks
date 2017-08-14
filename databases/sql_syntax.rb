0ddafb1a89b8f276975695ec13165dc46fe86b78
David Skaggs <davidtskaggs@gmail.com>
2017-08-14 09:11:22 -0400

    Add commands

--- databases/sql_syntax.rb
+++ databases/sql_syntax.rb
@@ -5,6 +5,20 @@ List of SQL Commands:
 - List available commands: .help
 - Exit: .exit
 - View tables: .tables
+- View table: SELECT * FROM <table name>;
+- View specifc values in table: SELECT <col name>, <add col name> FROM <table name>;
+- View specifc values in table: SELECT * FROM <table name> WHERE <boolean col name>="<true or false>";
+- Insert row: INSERT INTO <table name> (column data, column data,) VALUES (value, value);
+- Update row in table: UPDATE <table name> SET <col name>=<updated value> WHERE <col name>=<value>;
+- Create table: CREATE TABLE table_name (
+  attr_name DATATYPE,
+  attr_name DATATYPE,
+  attr_name DATATYPE
+);
+- Delete row in table: DELETE FROM <table name> WHERE <col name> = <value>;
+
+
+
 
 
 =end
