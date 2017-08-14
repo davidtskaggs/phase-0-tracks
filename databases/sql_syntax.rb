b7a686e1a1ebf525caad3954dcf726705a51b160
David Skaggs <davidtskaggs@gmail.com>
2017-08-14 09:04:45 -0400

    Add commands

---
+++ databases/sql_syntax.rb
@@ -0,0 +1,10 @@
+# 8.1: Intro to Databases
+=begin
+List of SQL Commands:
+- To run SQL in the console: sqlite3 <file_name.db>
+- List available commands: .help
+- Exit: .exit
+- View tables: .tables
+
+
+=end
