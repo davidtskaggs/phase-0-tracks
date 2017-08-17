58a2a1c04554a9abf1d0e8409890f6f87cb6f15a
David Skaggs <davidtskaggs@gmail.com>
2017-08-17 16:46:17 -0400

    add loop

--- databases/ruby-sql-project/file.rb
+++ databases/ruby-sql-project/file.rb
@@ -17,10 +17,38 @@ create_contacts_table = <<-SQL
 SQL
 
 contacts.execute(create_contacts_table)
-contacts.execute("INSERT INTO contacts(name, occupation, association, phone_number, email) VALUES ('tatiana donald', 'fashionista', 'family', '203.273.4706', 'a.t.k.donald@gmail.com') ")
+# contacts.execute("INSERT INTO contacts(name, occupation, association, phone_number, email) VALUES ('tatiana donald', 'fashionista', 'family', '203.273.4706', 'a.t.k.donald@gmail.com') ")
 
 contact_list = contacts.execute("SELECT * FROM contacts")
 # puts contact_list.class
-p contact_list
+# p contact_list
+
+
+
+
+puts "Welcome to your contact list"
+
+def user_decision(x)
+  loop do
+
+    puts "What would you like to do? (view, add, update, remove, or exit)"
+    user_input = gets.chomp.downcase
+    break if user_input == 'exit'
+
+    if user_input == 'view'
+      break
+    elsif user_input == 'add'
+      break
+    elsif user_input == 'update'
+      break
+    elsif user_input == 'remove'
+      break
+    else
+      puts "Invalid response. Please try again."
+    end
+  end
+end
+
+user_decision(contacts)
 
 
