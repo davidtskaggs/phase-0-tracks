e391393e6f1e3379e743993bc7b6b0588f0222f1
David Skaggs <davidtskaggs@gmail.com>
2017-08-17 16:22:01 -0400

    Add contacts creation program

--- databases/ruby-sql-project/file.rb
+++ databases/ruby-sql-project/file.rb
@@ -1,35 +1,23 @@
 require 'sqlite3'
 require 'faker'
 
-db = SQLite3::Database.new("match.db")
+contacts = SQLite3::Database.new("dskaggs_contacts.db")
 # db.results_as_hash = true
-db.execute("CREATE TABLE IF NOT EXISTS match(id INTEGER PRIMARY KEY, name TEXT, email TEXT, address_city TEXT, profession_industry TEXT)")
 
-def create_people (db, name, email, address_city, profession_industry)
-  db.execute("INSERT INTO match (name, email, address_city, profession_industry) VALUES (?, ?, ?, ?)", [name, email, address_city, profession_industry])
-end
+create_contacts_table = <<-SQL
+  CREATE TABLE IF NOT EXISTS contacts (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255),
+    occupation VARCHAR(255),
+    association VARCHAR(255),
+    phone_number VARCHAR(255),
+    email VARCHAR(255)
 
-# 10.times do
-#   create_people(db, Faker::Name.name, Faker::Internet.email, Faker::Address.city, Faker::Job.field)
+  )
+SQL
+
+contacts.execute(create_contacts_table)
+contacts.execute("INSERT INTO contacts(name, occupation, association, phone_number, email) VALUES ('tatiana donald', 'fashionista', 'family', '203.273.4706', 'a.t.k.donald@gmail.com') ")
+# def create_people (db, name, email, address_city, profession_industry)
+#   db.execute("INSERT INTO match (name, email, address_city, profession_industry) VALUES (?, ?, ?, ?)", [name, email, address_city, profession_industry])
 # end
-
-match = db.execute("SELECT * FROM match")
-# p match
-
-puts "Welcome to the Work - Finder List"
-# puts "Here are out current industries served: "
-# match.each do |value|
-#   puts "#{value[4]}"
-# end
-puts "Please enter industry:"
-user_input = gets.chomp.capitalize!
-
-match.each do |i|
-  industry = i[4]
-  if industry == user_input
-    puts "Id: #{i[0]}. Name: #{i[1]}"
-    puts "Location: #{i[3]}"
-    puts "Profession: #{i[4]}"
-    puts "------------------------"
-  end
-end
