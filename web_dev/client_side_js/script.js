f7035f3d866dd3663d8165dcaae4088133f1cbaa
David Skaggs <davidtskaggs@gmail.com>
2017-08-21 11:02:53 -0400

    Add js for release 2

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,3 +1,10 @@
 var heading = document.getElementsByTagName('h1');
 var heading = heading[0]
 heading.style.border = "2px solid blue";
+
+function HideImage(event) {
+  event.target.style.visibility = "hidden";
+}
+
+var photo = document.getElementsByTagName('img');
+photo[0].addEventListener("click", HideImage);
