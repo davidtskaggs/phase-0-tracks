1491f4989062647c6264e5e83c2fd12397f207eb
David Skaggs <davidtskaggs@gmail.com>
2017-08-22 11:17:27 -0400

    Add jquery feature

--- web_dev/jquery/index.js
+++ web_dev/jquery/index.js
@@ -1,3 +1,15 @@
+$('h1').hover(function() {
+  alert("Welcome! Let's learn some jQuery!")
+});
+
 $('li').on('click', function() {
   $(this).text("BOOM! jQuery Magic Sauce!").delay(800).fadeOut(400);
+});
+
+$("input").focus(function() {
+  $(this).css("background-color", "#c4c2c2")
+});
+
+$("input").blur(function() {
+  $(this).css("background-color", "#ffffff")
 });
